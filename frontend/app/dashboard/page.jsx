88cf4e7dc599889bd3f58d39b03af457cf811001
Adith Venkatesh <avenka29@ncsu.edu>
2025-02-09 00:02:25 -0500

    dashboard improvements

--- frontend/app/dashboard/page.jsx
+++ frontend/app/dashboard/page.jsx
@@ -6,6 +6,41 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar";
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from "@/components/ui/card";
+
+
+import { Calendar } from "@/components/ui/calendar";
+
+const data = 
+[
+  {
+    name: "todo",
+    title: "dashboard",
+    location: "todo",
+    start_date: "todo",
+    end_date: "todo",
+    deadline: "todo",
+    icon: Map,
+  },
+  {
+    name: "todo",
+    title: "dashboard",
+    location: "todo",
+    start_date: "todo",
+    end_date: "todo",
+    deadline: "todo",
+    icon: Map,
+  },
+
+
+
+]
 export default function Page() {
   return (
     (<SidebarProvider>
@@ -32,12 +67,42 @@ export default function Page() {
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <h1 className = "pl-1 font-bold">Upcoming Events</h1>
           <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
+              <div className="aspect-video rounded-md bg-muted/50">
+                  <Card>
+                    <CardHeader>
+                      <CardTitle>Quantitative Finance Meeting</CardTitle>
+                      <CardDescription>Club interest meeting for prospective members</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                      <p>Join our Quant Finance Interest Meeting to explore how math, coding...</p>
+                      <p className = "mt-2 text-sm underline"> Jake, Masarith, and 15 others</p>
+                    </CardContent>
+                  </Card>
+              </div>
+              <div className="aspect-video rounded-md bg-muted/50">
+                  <Card>
+                    <CardHeader>
+                      <CardTitle>Undergraduate Research Symposium</CardTitle>
+                      <CardDescription>Platform for students to present their research</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                      <p>Join our Undergraduate Research Symposium to explore innovative student research...</p>
+                      <p className = "mt-2 text-sm underline"> John, Sagnik, and 4 others</p>
+                    </CardContent>
+                  </Card>
+              </div>
+              <Calendar
+                mode="single"
+                selected={24}
+                // onSelect={setDate}
+                className="rounded-md border bg-gray"
+              />
           </div>
-          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
+              
+          </div>
         </div>
       </SidebarInset>
     </SidebarProvider>)
